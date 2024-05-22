3e531d1612c853caaa9cb2b98260bbb1f337dcb6
Marco Tulio <marcotulio270903@gmail.com>
2024-05-21 23:39:43 -0300

    Initial commit

--- dot_config/doom/snippets/eshell-mode/dot_yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `eshell-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'eshell-mode
-                     '(("for" "for f in ${1:*} { ${2:echo} \"$f\"; $3} $0" "Eshell for loop" nil nil nil "/home/marco_normal/.config/doom/snippets/eshell-mode/for" nil nil)))
-
-
-;;; Do not edit! File generated at Mon Oct 16 12:54:04 2023
--- dot_config/doom/snippets/matlab-mode/dot_yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `matlab-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'matlab-mode
-                     '(("switch" "switch ${1:variable}\n    case ${2:value}\n    $3\n    otherwise\nend\n    " "switch" nil nil nil "/home/marco_normal/.config/doom/snippets/matlab-mode/switch" nil nil)))
-
-
-;;; Do not edit! File generated at Mon Oct 16 12:54:04 2023
